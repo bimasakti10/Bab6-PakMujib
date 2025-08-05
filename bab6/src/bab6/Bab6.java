dc9d129b0309919666d630dd5a3eea99a27cc3de
Bima Sakti <probima20@gmail.com>
2025-08-05 13:56:31 +0700

    Add files via upload

---
+++ bab6/src/bab6/Bab6.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP EliteBook 840 G5
+ */
+public class Bab6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//            String temp = JOptionPane.showInputDialog(null,
+//            "Masukan Angka", "Menentukan Ganjil Genap",
+//            JOptionPane.QUESTION_MESSAGE);
+//    int input =
+//            Integer.parseInt(temp);
+//    System.out.println((input %
+//            2)); if ((input % 2) == 0) {
+//                JOptionPane.showMessageDialog(null, "Angka : "
+//                        + input + " "
+//                                + "adalah bilangan GENAP", "Informasi",
+//                                JOptionPane.INFORMATION_MESSAGE);
+//            } else if ((input % 2) == 1) {
+//                JOptionPane.showMessageDialog(null, "Angka : "
+//                        + input + " "
+//                                + "adalah bilangan GANJIL", "Informasi",
+//                                JOptionPane.INFORMATION_MESSAGE);
+//   }
+
+//   String umur = JOptionPane.showInputDialog(null, "Kamu umur berapa ?");
+//   if (Integer.parseInt(umur) <= 5) {
+//       JOptionPane.showMessageDialog(null, "Balita");
+//   }
+//    else if (Integer.parseInt(umur) >= 6 && Integer.parseInt(umur) <= 11)
+//    { JOptionPane.showMessageDialog(null, "Anak - anak");
+//    
+//    }
+//    else if (Integer.parseInt(umur) >= 12 && Integer.parseInt(umur) <= 25)
+//    { JOptionPane.showMessageDialog(null, "Remaja");
+//    }else{ 
+//JOptionPane.showMessageDialog(null, "Dewasa");
+//   }
+
+
+//    String nilai = JOptionPane.showInputDialog
+//        (null, "Masukan nilai :");
+//    switch (nilai.toUpperCase()) {
+//        case "A":
+//            JOptionPane.showMessageDialog(null, "Sangat baik.");
+//            break;
+//        case "B":
+//            JOptionPane.showMessageDialog(null, "Baik.");
+//            break;
+//        case "C":
+//            JOptionPane.showMessageDialog(null, "Cukup.");
+//        case "D":
+//           JOptionPane.showMessageDialog(null, "Jelek.");
+//            break;
+//        default :
+//            JOptionPane.showMessageDialog(null,
+//                    "Salah memasukan nilai !");
+//}
+//    for (int a = 2; a < 12; a += 2) {
+//        System.out.println("Bilanga Genap : " + a);
+  
+//    int baris = 5;
+//
+//    for (int i = 0; i <= baris; i++) {
+//
+//        for (int j = 0; j < i; j++) {
+//            System.out.print("* ");
+//}
+//
+//        System.out.println("");
+    
+//    int jumlahKelas =
+//            Integer.parseInt(JOptionPane.showInputDialog(null,
+//                    "Masukan jumlah kelas : ","Input",
+//                    JOptionPane.INFORMATION_MESSAGE));
+//    String isiKelas[][] = new
+//            String[jumlahKelas][];
+//    for (int kelas = 0; kelas < jumlahKelas; kelas++) {
+//        String namaKelas = JOptionPane.showInputDialog(null, "Nama"
+//                + "Kelas :", "Masukan Nama Kelas",
+//                JOptionPane.INFORMATION_MESSAGE);
+//        int jumlahKursi =
+//                Integer.parseInt(JOptionPane.showInputDialog(null, "Jumlah"
+//                        + "murid masing - masing kelas : ","Input",
+//                        JOptionPane.INFORMATION_MESSAGE));
+//        isiKelas[kelas] = new
+//                String[jumlahKursi];
+//        System.out.println("Kelas " + namaKelas
+//                + " : "); for (int kursi = 0; kursi <
+//                        jumlahKursi; kursi++) {
+//                    String namaSiswa = JOptionPane.showInputDialog(null,
+//                            "Nama Siswa", "Kelas " + namaKelas,
+//                            JOptionPane.INFORMATION_MESSAGE); isiKelas[kelas][kursi]
+//                                    = namaSiswa;
+//                            System.out.println("Nama Siswa " +
+//                                    isiKelas[kelas][kursi]);
+//                }
+//                System.out.println("----------------------------------------"
+//                        + "");
+   
+    
+//int arr[] = new int[4];
+//boolean hasil = true;
+//arr[0] = 2;
+//arr[1] = 5;
+//arr[2] = 7;
+//arr[3] = 1;
+//while (hasil == true) {
+//String input = JOptionPane.showInputDialog(null,
+//"masukan bilangan bulat :");
+//int cekInputan = Integer.parseInt(input);
+//for (int a = 0; a < 4; a++) {
+//if (cekInputan == arr[a]) {
+//hasil = false;
+//break;
+//} else {
+//hasil = true;
+//}
+//}
+//if (hasil == true) {
+//JOptionPane.showMessageDialog(null, "Angka " + cekInputan
+//+ " tidak ada di dalam array.");
+//
+//} else {
+//JOptionPane.showMessageDialog(null, "Angka " + cekInputan
+//+ " ada di dalam array.");
+//}
+//}
+
+//    String input = "";
+//int cekbil = 0;
+//do{
+//input = JOptionPane.showInputDialog(null, "4 x 3 :");
+//cekbil = Integer.parseInt(input);
+//}while(cekbil != 12);
+
+}
+}
+
